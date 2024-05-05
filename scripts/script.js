0606151c4954365fcc13a7103029effc084608ef
LUCAS\lucas <lucasrodriguesdonascimento@outlook.com>
2024-05-05 05:34:13 -0300

    portf

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const idBtn = document.getElementById('id-btn');
+const enableTextExperience = document.getElementById('experience-text');
+const disableTextAboutMe = document.getElementById('about-me-text');
+const titleCard = document.getElementById('title-card');
+const emailBtn = document.getElementById('email-btn');
+
+emailBtn.addEventListener('click', () => {
+    window.location.href = 'mailto:lucasrodriguesdonascimento@outlook.com?subject=Olá%20do%20meu%20portfólio';
+});
+let isClicked = false;
+idBtn.addEventListener('click', (event) => {
+    if (!isClicked) {
+        disableTextAboutMe.style.display = "none";
+        enableTextExperience.style.display = "block";
+        isClicked = true;
+        idBtn.textContent = "Sobre mim";
+        titleCard.textContent = 'Minhas Experiências'
+        return;
+    }
+    disableTextAboutMe.style.display = "block";
+    enableTextExperience.style.display = "none";
+    idBtn.textContent = "Experiências";
+    titleCard.textContent = 'Sobre mim'
+    isClicked = false;
+});
